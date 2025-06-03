8beb91b6da1341f1b01c2cadcb186be5e06664e1
Jacob-Idolor <jacob.idolor@outlook.com>
2025-06-03 16:21:33 -0700

    Remove duplicate import

--- Dynatrace/app/core/config.py
+++ Dynatrace/app/core/config.py
@@ -1,10 +1,7 @@
 from pydantic_settings import BaseSettings
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
-from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     dynatrace_api_url: str
